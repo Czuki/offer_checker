f4c33d84148e841eb1f43406a8277bb6e8298978
czuki <bart3322@gmail.com>
2022-03-15 22:53:58 +0100

    simple dashboard

--- offer_checker/urls.py
+++ offer_checker/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import checker.views as checker_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', checker_views.DashboardView.as_view())
 ]
