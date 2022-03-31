1d9d2db83c67fd195667d3857361e848d47a0d4e
czuki <bart3322@gmail.com>
2022-03-31 22:44:00 +0200

    settings updated

--- offer_checker/settings.py
+++ offer_checker/settings.py
@@ -134,6 +134,9 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
